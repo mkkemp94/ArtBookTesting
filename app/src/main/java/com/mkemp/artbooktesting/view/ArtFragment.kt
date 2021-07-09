c6f7eb0982054f9aa568e425da3322b8602e52f3
MKemp <mkemp@accu-time.com>
2021-07-09 10:52:27 -0400

    Navigate from ArtFragment to ArtDetailsFragment

--- app/src/main/java/com/mkemp/artbooktesting/view/ArtFragment.kt
+++ app/src/main/java/com/mkemp/artbooktesting/view/ArtFragment.kt
@@ -3,12 +3,29 @@ package com.mkemp.artbooktesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.mkemp.artbooktesting.R
+import com.mkemp.artbooktesting.databinding.FragmentArtsBinding
 
 class ArtFragment : Fragment(R.layout.fragment_arts)
 {
+    private var fragmentBinding: FragmentArtsBinding? = null
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+        
+        val binding = FragmentArtsBinding.bind(view)
+        fragmentBinding = binding
+        
+        binding.fab.setOnClickListener {
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
+    }
+    
+    override fun onDestroyView()
+    {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 }
