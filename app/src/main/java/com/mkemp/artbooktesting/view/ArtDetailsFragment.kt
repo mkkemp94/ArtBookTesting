7f271d0fa1f835ae5919bb1b4106e42d255fc28e
MKemp <mkemp@accu-time.com>
2021-07-10 13:45:14 -0400

    Finish hooking up fragments? (there are some bugs)

--- app/src/main/java/com/mkemp/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/mkemp/artbooktesting/view/ArtDetailsFragment.kt
@@ -2,23 +2,32 @@ package com.mkemp.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.mkemp.artbooktesting.R
 import com.mkemp.artbooktesting.databinding.FragmentArtDetailsBinding
+import com.mkemp.artbooktesting.util.Status
+import com.mkemp.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtDetailsFragment @Inject constructor(
         val glide: RequestManager
 ) : Fragment(R.layout.fragment_art_details)
 {
+    lateinit var viewModel : ArtViewModel
+    
     private var fragmentBinding: FragmentArtDetailsBinding? = null
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+        
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
         
         val binding = FragmentArtDetailsBinding.bind(view)
         fragmentBinding = binding
@@ -40,6 +49,45 @@ class ArtDetailsFragment @Inject constructor(
         }
         
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+        
+        binding.saveButton.setOnClickListener {
+            viewModel.makeArt(
+                    binding.nameText.text.toString(),
+                    binding.artistText.text.toString(),
+                    binding.yearText.text.toString()
+            )
+        }
+    }
+    
+    private fun subscribeToObservers()
+    {
+        viewModel.selectedImageUrl.observe(viewLifecycleOwner, Observer { url ->
+            fragmentBinding?.let {
+                glide.load(url).into(it.artImageView)
+            }
+        })
+        
+        viewModel.insertArtMessage.observe(viewLifecycleOwner, Observer {
+            when (it.status)
+            {
+                Status.SUCCESS ->
+                {
+                    Toast.makeText(requireContext(), "Success", Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                    viewModel.resetInsertArtMsg()
+                }
+    
+                Status.ERROR ->
+                {
+                    Toast.makeText(requireContext(), it.message ?: "Error", Toast.LENGTH_LONG).show()
+                }
+    
+                Status.LOADING ->
+                {
+                
+                }
+            }
+        })
     }
     
     override fun onDestroyView()
--- app/src/main/java/com/mkemp/artbooktesting/view/ArtFragment.kt
+++ app/src/main/java/com/mkemp/artbooktesting/view/ArtFragment.kt
@@ -3,10 +3,15 @@ package com.mkemp.artbooktesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mkemp.artbooktesting.R
 import com.mkemp.artbooktesting.adapter.ArtRecyclerAdapter
 import com.mkemp.artbooktesting.databinding.FragmentArtsBinding
+import com.mkemp.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtFragment @Inject constructor(
@@ -14,17 +19,48 @@ class ArtFragment @Inject constructor(
 ): Fragment(R.layout.fragment_arts)
 {
     private var fragmentBinding: FragmentArtsBinding? = null
+    lateinit var viewModel: ArtViewModel
+    
+    private val swipeCallBack = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+    {
+        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean
+        {
+            return true
+        }
+    
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int)
+        {
+            val layoutPosition = viewHolder.layoutPosition
+            val selectedArt = artRecyclerAdapter.arts[layoutPosition]
+            viewModel.deleteArt(selectedArt)
+        }
+    }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
         
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+        
         val binding = FragmentArtsBinding.bind(view)
         fragmentBinding = binding
+        
+        subscribeToObservers()
+        
+        binding.recyclerViewArt.adapter = artRecyclerAdapter
+        binding.recyclerViewArt.layoutManager = LinearLayoutManager(requireContext())
+        ItemTouchHelper(swipeCallBack).attachToRecyclerView(binding.recyclerViewArt)
         
         binding.fab.setOnClickListener {
             findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
         }
+    }
+    
+    private fun subscribeToObservers()
+    {
+        viewModel.artList.observe(viewLifecycleOwner, {
+            artRecyclerAdapter.arts = it
+        })
     }
     
     override fun onDestroyView()
